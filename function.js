e5e4ff01bf11efea4f3c5336b59d0c575f374b37
SPking1112 <rajasriveelanbot.com>
2025-02-26 14:11:08 +0530

    popup

--- function.js
+++ function.js
@@ -1,8 +1,10 @@
-
+let pop = document.getElementById("popup");
+pop.style.display="none"
+console.log()
 
 // Cart page Redirect
 function cart(){
-    
+    pop.style.display="block"
 }
 
 // Shop page Redirect
