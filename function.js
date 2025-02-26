2ae6c593a0f43566e62b2182ea2b086457734498
SPking1112 <rajasriveelanbot.com>
2025-02-26 09:48:35 +0530

    Popup added

--- function.js
+++ function.js
@@ -2,12 +2,12 @@
 
 // Cart page Redirect
 function cart(){
-    window.location.href="./cart.html"
+    
 }
 
 // Shop page Redirect
 function shop(){
-    window.location.href="./shop.html"
+    window.location.href="./shop.html";
 }
 
 
