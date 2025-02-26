49fb79f21504180a1627608b1fac905e06ac2551
SPking1112 <rajasriveelanbot.com>
2025-02-26 07:58:41 +0530

    Added Login Page

--- function.js
+++ function.js
@@ -1,1 +1,15 @@
-console.log(window.location)
+
+
+// Cart page Redirect
+function cart(){
+    window.location.href="./cart.html"
+}
+
+// Shop page Redirect
+function shop(){
+    window.location.href="./shop.html"
+}
+
+
+
+
